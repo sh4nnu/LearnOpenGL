b1eb5bce861d755b560cff5f57f96f729a0910ea
sh4nnu <manikishanghantasala@gmail.com>
2021-12-17 16:17:00 +0530

    Add  Lab1: Window

--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -4,11 +4,62 @@
 //
 //  Created by Kishan on 15/12/21.
 //
-
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+void framebuffer_size_callback(GLFWwindow* window,  int width, int height) {
+    glViewport(0,0, width, height);
+}
+
+void  processInput(GLFWwindow* window) {
+    //close on  esc key press
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+int main() {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    
+    
+    GLFWwindow* window  =  glfwCreateWindow(800, 600, "Learn OpenGL", NULL, NULL);
+    if(window ==  NULL)  {
+        std::cout  << "Failed to create a GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    //check for glad only  after  GL context is created.
+    
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+    
+    glViewport(0, 0, 800, 600);
+
+    // callback function to change viewport when window size changes.
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    
+    while(!glfwWindowShouldClose(window))  {
+        // input
+        processInput(window);
+        
+        // rendering magic
+           // color
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        // check polls and swap buffers
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+    glfwTerminate();
     return 0;
 }
